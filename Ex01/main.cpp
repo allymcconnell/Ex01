11263481f0eee5f7aa6cac956715e709cd998e68
allymcconnell <amcconnell16@my.whitworth.edu>
2016-05-25 10:03:51 -0700

    first commit

--- Ex01/main.cpp
+++ Ex01/main.cpp
@@ -6,10 +6,57 @@
 //  Copyright © 2016 Ally McConnell. All rights reserved.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+#include <iostream>
+using namespace std;
+
+int ex01();
+
+int main() {
+    ex01();
 }
+
+int ex01()
+{
+    //section A
+    bool hasPassedTest = true;
+    
+    //section B
+    int x = rand();
+    int y = rand();
+    
+    if ( x > y )
+    {
+        cout << " x is bigger than y" << endl;
+    }
+    //section C
+    int numberOfShares;
+    cout << "Please enter an integer value: ";
+    cin >> numberOfShares;
+    if ( numberOfShares < 100)
+        cout << "i is less than 100";
+    
+    //section D
+    int box;
+    cout << "Enter box width: ";
+    cin >> box;
+    int book;
+    cout << "Enter book width: ";
+    cin >> book;
+    
+    if (box % book == 0)
+    {
+        cout << "Box is divisible";
+    }
+        
+        
+        
+        
+    
+    
+    
+    
+    
+}
+
